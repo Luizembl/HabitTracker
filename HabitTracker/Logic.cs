44a452195240e98a2ba8c132fe4f12a51d37f0e5
Luiz Eduardo M B Leal <luizmbl@hotmail.com>
2023-04-17 23:00:41 -0700

    Menu and logic class

--- HabitTracker/Logic.cs
+++ HabitTracker/Logic.cs
@@ -1,11 +1,71 @@
-﻿using System;
+﻿using System.Text;
+using Microsoft.Data.Sqlite;
+
 namespace HabitTracker
 {
-	public class Logic
+	internal class Logic
 	{
-		public Logic()
-		{
-		}
-	}
+        static string connectionDataBase = "Data Source=HabitTrackerApp.db";
+
+        internal void CreateDB()
+        {
+            using (var connection = new SqliteConnection(connectionDataBase))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS books_read (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+                tableCmd.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        internal static void GetAllRecords()
+        {
+            Console.Clear();
+
+            using (var connection = new SqliteConnection(connectionDataBase))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    $"SELECT * FROM books_read ";
+
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+            }
+        }
+
+        internal static void InsertRecord()
+        {
+            Console.Clear();
+
+            using (var connection = new SqliteConnection(connectionDataBase))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                Console.WriteLine("How many books did you read?: ");
+                int booksQty = int.Parse(Console.ReadLine());
+                Console.WriteLine("What the date? yyyy-mm?: ");
+                string date = Console.ReadLine();
+
+                SqliteCommand insertHabit = new SqliteCommand("INSERT INTO books_read (Quantity, Date) VALUES (@booksQty, @date)", connection);
+                insertHabit.Parameters.AddWithValue("@Quantity", booksQty);
+                insertHabit.Parameters.AddWithValue("@Date", date);
+
+                insertHabit.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+
+    }
 }
 
